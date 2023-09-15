82013e5e31e401de973b8348d15df004c738bbba
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-15 12:18:51 -0400

    add theme toggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,14 @@
 
 import styles from "./themeToggle.module.css";
+import Image from "next/image";
 const ThemeToggle = () => {
-    return <div className={styles.container}>ThemeToggle</div>;
+    return <div className={styles.container}>
+
+        <Image className={styles.margin} src="/moon.png" alt="dark mode" width={24} height={24} />
+        <div className={styles.ball}></div>
+        <Image className={styles.margin} src="/sun.png" alt="light mode" width={24} height={24} />
+
+    </div>;
 };
 
 export default ThemeToggle;
