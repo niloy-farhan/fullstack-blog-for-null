41103af8f2d8a2e917565a553e3068e008f65967
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-17 08:02:37 -0400

    add categories

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,12 +1,67 @@
 import React from 'react'
 import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () =>  {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Categories</h1>
+            <h1 className={styles.title}>categories</h1>
             <div className={styles.categories}>
-                <div className={styles.category}></div>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src={"/style.png"}
+                        alt={""}
+                        width={32}
+                        height={32}
+                        className={styles.image}
+                    />
+                    thoughts
+                </Link>
+
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src={"/style.png"}
+                        alt={""}
+                        width={32}
+                        height={32}
+                        className={styles.image}
+                    />
+                    articles
+                </Link>
+
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src={"/style.png"}
+                        alt={""}
+                        width={32}
+                        height={32}
+                        className={styles.image}
+                    />
+                    infiltration
+                </Link>
+
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src={"/style.png"}
+                        alt={""}
+                        width={32}
+                        height={32}
+                        className={styles.image}
+                    />
+                    programs
+                </Link>
+
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image
+                        src={"/style.png"}
+                        alt={""}
+                        width={32}
+                        height={32}
+                        className={styles.image}
+                    />
+                    projects
+                </Link>
             </div>
         </div>
     )
