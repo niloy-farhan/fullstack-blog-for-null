c97eb9bec5b25f2ddb4b40ba9cdcd52d5fb1a30a
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-17 04:43:42 -0400

    update

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,7 +3,19 @@ import styles from "./featured.module.css";
 
 const Featured = () =>  {
     return (
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.fh1}>who am i?</h1>
+            <div className={styles.fp}>
+                <p>
+                    i'm a <strong className={styles.str}>monk, </strong>
+                    who's a<strong className={styles.str}> philosopher, </strong> passionate about building <strong>programs and electronics,</strong> find's <strong className={styles.str}>physics</strong> fun. specially quantum and astrophysics. currently learning about quantum computers and <strong>researching</strong> a way you can simulate real-world object in the real world with the help of <strong>quantum computation and engineering</strong>. in this blog i post weird stuff! mostly my <strong>thoughts</strong> and philosophies
+                </p>
+            </div>
+            <div className={styles.buttn}>
+                <buton className={styles.butx}>more me</buton>
+            </div>
+
+        </div>
     )
 }
 
