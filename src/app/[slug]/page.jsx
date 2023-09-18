9182f7c97eb4d3359cf0b545c7091037feffa0cd
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-18 02:16:36 -0400

    container

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,9 +1,30 @@
 import styles from "./singlePage.module.css"
+import Image from "next/image";
 
 const SinglePage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.infoContainer}></div>
+            <div className={styles.infoContainer}>
+                <div className={styles.textContainer}>
+                    <h1> feature 1</h1>
+                    <div className={styles.user}>
+                        <div className={styles.userImageContainer}>
+                            <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                        </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.username}>
+                                null
+                            </span>
+                            <span className={styles.date}>
+                                01.01.2023
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                </div>
+            </div>
             <div className={styles.content}>
                 <div className={styles.post}>
 
