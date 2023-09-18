22e7cd41b7f331e4a895a2340290eb79b240019f
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-09-18 01:56:03 -0400

    container

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -2,7 +2,15 @@ import styles from "./singlePage.module.css"
 
 const SinglePage = () => {
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <div className={styles.infoContainer}></div>
+            <div className={styles.content}>
+                <div className={styles.post}>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
